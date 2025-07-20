cc47c671dac4c253ca4ee7d7f6b98046de4ff443
leadxSASI <nilanthirenuka106@gmail.com>
2025-07-20 20:41:01 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "lw4H2DAD#e7W5lyXxZKpVRoJHWXdMuyvw1xXwYQeQPVugLkBwRUA",
+SESSION_ID: process.env.SESSION_ID || "",
 ALIVE_IMG: process.env.ALIVE_IMG || "Enter The Image URL",
 ALIVE_MSG: process.env.ALIVE_MSG || "HELLO IM SASI MD CREATED BY Leadx SASI <NOW ALIVE> ",
 };
