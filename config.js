d32f1502ef63b5f8b89648b2b1dc068a867b5de2
leadxSASI <nilanthirenuka106@gmail.com>
2025-09-02 18:13:00 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "levanter_21a33b10a38af461ca185df60c6045787",
 ALIVE_IMG: process.env.ALIVE_IMG || "Enter The Image URL",
 ALIVE_MSG: process.env.ALIVE_MSG || "HELLO IM SASI MD CREATED BY Leadx SASI <NOW ALIVE> ",
 };
